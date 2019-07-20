7900b8e3f62e2a89b22dd3899845a7e1771c96bc
dwlam8790 <49571438+dwlam8790@users.noreply.github.com>
2019-07-20 17:40:13 -0400

    added var to js

---
+++ assets/javascript/index.js
@@ -0,0 +1,10 @@
+var rightAnswers = 0;
+var wrongAnswers = 0;
+var noAnswers = 0;
+
+//questions for trivia game
+//using an array to select correct answer
+//20 questions?
+var questions = [{
+
+}]
