2003014f467112473e5717ac8be8da69e1677edb
dwlam8790 <49571438+dwlam8790@users.noreply.github.com>
2019-07-20 18:18:18 -0400

    added the first questions to js with array

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -4,7 +4,10 @@ var noAnswers = 0;
 
 //questions for trivia game
 //using an array to select correct answer
-//20 questions?
+//20 questions? -- I only did 10
 var questions = [{
-
+    question:
+    answersListed:
+    answer:
+    //place array number
 }]
